08591656d3cbf988b304ec66aa9fb8db3bd6fb41
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2025-01-23 11:49:26 +0100

    219 current  2025-01-23 11:49:23  25.05.20250108.4bc9c90  6.6.69                          *

--- configuration.nix
+++ configuration.nix
@@ -60,6 +60,7 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.drivers = [ pkgs.hplip ];
 
   # Enable sound with pipewire.
   # sound.enable = true;
