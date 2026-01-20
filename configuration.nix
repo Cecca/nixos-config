8800ff1ddbc23b43d316122b227374b9a6a35e23
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2026-01-20 18:45:12 +0100

    start using kanata

--- configuration.nix
+++ configuration.nix
@@ -81,6 +81,63 @@
     #media-session.enable = true;
   };
 
+  #######################################################################
+  # Keyboard remapper
+  # https://dev.to/shanu-kumawat/how-to-set-up-kanata-on-nixos-a-step-by-step-guide-1jkc
+
+  # Enable the uinput module
+  boot.kernelModules = ["uinput"];
+
+  # Enable uinput
+  hardware.uinput.enable = true;
+  # Set up udev rules for uinput
+  services.udev.extraRules = ''
+    KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+  '';
+
+  # Ensure the uinput group exists
+  users.groups.uinput = {};
+
+  # Add the Kanata service user to necessary groups
+  systemd.services.kanata-internalKeyboard.serviceConfig = {
+    SupplementaryGroups = [
+      "input"
+      "uinput"
+    ];
+  };
+
+  services.kanata = {
+    enable = true;
+    keyboards = {
+      internalKeyboard = {
+        devices = [
+          "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
+        ];
+        config = ''
+          (defsrc
+           caps d f h j k l
+          )
+          (defvar
+           tap-time 200
+           hold-time 200
+          )
+          (defalias
+           caps (tap-hold 200 200 esc lctl)
+           jctrl (tap-hold 200 200 j rctl)
+           arr (tap-hold $tap-time $hold-time f (layer-toggle arrow))
+           del del  ;; Alias for the true delete key action
+          )
+          (deflayer base
+           @caps d @arr h @jctrl k l
+          )
+          (deflayer arrow
+           _ _ @del left down up right
+          )
+        '';
+      };
+    };
+  };
+
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
